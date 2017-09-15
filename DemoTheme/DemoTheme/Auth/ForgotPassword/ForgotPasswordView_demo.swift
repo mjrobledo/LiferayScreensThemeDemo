81d639d29999ac93e5c6e9e55f5449a8dd77f87b
Miguel Robledo <mrobledo@lisyx.com>
2017-09-15 16:21:55 -0500

    v8

---
+++ DemoTheme/DemoTheme/Auth/ForgotPassword/ForgotPasswordView_demo.swift
@@ -0,0 +1,22 @@
+//
+//  ForgotPasswordView_demo.swift
+//  LiferayScreensThemeDemo
+//
+//  Created by MacBook Pro on 15/09/17.
+//  Copyright © 2017 Liferay. All rights reserved.
+//
+
+import UIKit
+import LiferayScreens
+
+@IBDesignable open class  ForgotPasswordView_demo: ForgotPasswordView_default {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
